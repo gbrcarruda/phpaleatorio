a148e42fa186f38c88db13cdb501a7cfc6498261
GABRIEL ARRUDA <gbrcarruda@gmail.com>
2019-04-19 10:21:27 -0300

    Início dos testes com FOR

---
+++ testefor.php
@@ -0,0 +1,23 @@
+<?php
+
+//PRÁTICA COM ESTRUTURA DE REPETIÇÃO "FOR"
+
+//$j=6;
+//$i=0;
+//$cont=0;
+
+for ($i = 0; $i < 10; $i++){
+
+    echo "oi";
+}
+
+for ($i = 0; $i < 10; $i++){
+
+    echo "tchau";
+
+}
+$cont = 10;
+for ($i = $cont; $i > 0; $i--){
+
+    echo "z";
+    }
